174e4fad3cf257883312d9826116d36f55cff770
paisabol9 <90238296+paisabol9@users.noreply.github.com>
2023-08-06 03:04:57 +0000

    Added more files and updated text

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def hello_world():
+def index():
   return render_template('index.html')
 
+@app.route("/one_test")
+def one_test():
+  return render_template('one.html')
+
+@app.route("/directory_ria")
+def ria():
+  return render_template('/directory/ria.html')
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", debug=True)
